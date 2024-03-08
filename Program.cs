b5c601feee8379f17afea3116bfe11e2f4e04236
tanjirhc <130759310+tanjirhc@users.noreply.github.com>
2024-03-08 11:01:17 +0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Beecrowd_1004_Product
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int A = int.Parse(Console.ReadLine());
+
+            int B = int.Parse(Console.ReadLine());
+
+            int PROD = A * B;
+
+            Console.WriteLine("PROD = {0}", PROD);
+            Console.ReadKey();
+        }
+    }
+}
